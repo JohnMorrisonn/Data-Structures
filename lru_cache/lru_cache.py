8f81d97a92afe4062ce5fa96671444968fcbb868
John Morrison <jjosephmorrison@gmail.com>
2020-01-28 18:40:43 -0500

    LRU

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,7 @@
+from doubly_linked_list import DoublyLinkedList
+from doubly_linked_list import ListNode
+
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -7,7 +11,11 @@ class LRUCache:
     to every node stored in the cache.
     """
     def __init__(self, limit=10):
-        pass
+        self.limit = limit
+        self.size = 0
+        self.dll = DoublyLinkedList()
+        self.storage = {}
+
 
     """
     Retrieves the value associated with the given key. Also
@@ -17,7 +25,17 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        pass
+        
+        if self.size == 0:
+            return
+        
+        else:
+            if key in self.storage.keys():
+                node = self.storage[key]
+                self.dll.move_to_end(node)
+                return node.value[1]
+            else:
+                return
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -30,4 +48,22 @@ class LRUCache:
     the newly-specified value.
     """
     def set(self, key, value):
-        pass
+        
+        # If dict key already exists
+        if key in self.storage:
+            node = self.storage[key]
+            node.value = (key, value)
+            self.dll.move_to_end(node)
+        else:
+            # If cache limit reached
+            if self.size == self.limit:
+            
+                # Remove from dll and storage
+                del self.storage[self.dll.head.value[0]]
+                self.dll.remove_from_head()
+                self.size -=1
+                ## Add new key-values to storage and dll
+
+            self.dll.add_to_tail((key, value))
+            self.storage[key] = self.dll.tail
+            self.size += 1
